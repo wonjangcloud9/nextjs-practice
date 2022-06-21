6934d1e0fa9265341d316b53998e7a807104a2e6
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-06-21 23:58:27 +0900

    SSR

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,11 @@
-export default function Potato() {
-  return "about";
+import { useState } from "react";
+
+export default function Home() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <h1>Hello {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+    </div>
+  );
 }
